5444e0de9a720812391105fc6175b3c95cafe5c2
mahdi-chavoshi <chavoshiiiiiiii@gmail.com>
2022-09-06 16:31:57 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+const labels = [
+  "",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "June",
+  "June",
+  "June",
+  "June",
+  "June",
+  "June",
+];
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      backgroundColor: "#147AD6",
+      borderColor: "#147AD6",
+      data: [500, 510, 480, 120, 500, 510, 480, 150, 500, 510, 480, 100, 500],
+    },
+  ],
+};
+
+const config = {
+  type: "line",
+  data: data,
+  options: {
+    responsive: true,
+    scales: {
+      x: {
+        display: false,
+        title: {
+          display: true,
+        },
+      },
+      y: {
+        display: true,
+      },
+    },
+    plugins: {
+      title: {
+        display: false,
+      },
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        mode: "index",
+        intersect: false,
+      },
+    },
+    hover: {
+      mode: "index",
+      intersec: false,
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+          drawBorder: false,
+          color: "white",
+          drawOnChartArea: true,
+          drawTicks: false,
+        },
+        display: true,
+      },
+      y: {
+        min: 0,
+        max: 750,
+        ticks: {
+          // forces step size to be 50 units
+          stepSize: 250,
+        },
+      },
+    },
+  },
+};
+
+const myChart = new Chart(document.getElementById("myChart"), config);
